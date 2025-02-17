c30980e6f77fc4ec9b1330f68737b1688291b43e
gonzalovivares <163347111+gonzalovivares@users.noreply.github.com>
2025-02-17 10:42:15 +0100

    Update example_simulation_DMIrate.py

--- Intake_patterns/example_simulation_DMIrate.py
+++ Intake_patterns/example_simulation_DMIrate.py
@@ -6,6 +6,9 @@ import numba
 from numba import jit, njit, types
 from numba.typed import List
 
+# The model is run using the package DiffEqPy, which requires the installation of Julia. 
+# Other alternative solver in Python can be solve_ivp from SciPy, although simulation time is longer.
+# The simulation of this feeding pattern uses to be stiff, so small timesteps (< 0.001) are recommended.
 from diffeqpy import de
 import time
 from datetime import timedelta
@@ -17,8 +20,10 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
         decimal_part = x - integer_part
         return np.array([integer_part, decimal_part])
     feed = state_vars[0]
-    #  print(state_vars, feed)
 
+    # Extract model inputs from list 'inputs'. The order of columns in the Dataframe model_inputs
+    # outside this function must be the same as the list input_names defined below. This calling method
+    # was required after implementing numba for simulation speed.
     input_vals = inputs
     input_names = ['drop', 'pulse_dm', 'dm_availability', 'n_meals', 'meal_len', 'n_feedings' ]
 
@@ -29,10 +34,8 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
     regime = input_vals[input_names.index('dm_availability')]
     n_feedings = input_vals[input_names.index('n_feedings')]
 
-    # n_day, decimal_day = extract_integer_decimal(t)
-
+    # Function that defines the meal distribution throughout the day.
     def meal_scheme(t, n_meals, meal_len, pulse_i, drop, regime, n_feedings):
-        #  pulse_i = inputs['pulse_dm']
         n_day, decimal_day = extract_integer_decimal(t)
         k_drop = drop
         meal_len_extra = meal_len
@@ -40,15 +43,20 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
         end_time_meal = meal_len * 0.8
         big_meal_extension = 3.05 - 0.1375 * n_meals
         n_feedings = n_feedings
+
+        # The function allows a maximum of 4 feedings per day.
+        # Regime = 1 refers to restricted feeding, whereas regime = 0 refers to ad libitum feeding.
         if regime == 1:
-            if n_feedings == 1:
-                long_meal_1 = 1
-                long_meal_2 = -50
+            if n_feedings == 1:     # If 1 feeding per day at restricted, first meal will be longer than the rest. Later on, the algorithm will ignore the -50.
+                long_meal_1 = 1     # This refers to the number of the meal.
+                long_meal_2 = -50   # Later on, the algorithm will ignore the -50.
                 long_meal_3 = -50
                 long_meal_4 = -50
-            elif n_feedings == 2:
+            elif n_feedings == 2:   # If 2 feedings per day at restricted, first meal and the meal lying in the middle of the day
+                                    # will be longer than the rest.
                 long_meal_1 = 1
-                long_meal_2 = np.ceil(n_meals / n_feedings) + 1
+                long_meal_2 = np.ceil(n_meals / n_feedings) + 1     # This refers to the number of the meal.
+                                                                    # If 8 meals per day and 2 feedings, the meal 5 will be longer.
                 long_meal_3 = -50
                 long_meal_4 = -50
             elif n_feedings == 3:
@@ -83,8 +91,17 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
             long_meal_3 = -50
             long_meal_4 = -50
 
-
+        # This function defines the times where meals occur in a day, describing the moments where 'pulse' is active
         def meal_definition_restricted(decimal_day, n_meals, meal_len):
+
+            # The function can simulate up to 15 meals in a day.
+            # As the simulation of the model is in days, we extract its decimal component to compare it with the position 
+            # of each meal in the day.
+
+            # If the fraction of the day (time, at each timestep) is between the time of the start of a meal, defined by
+            # (decimal_day < 1 / (n_meals + 1), and the end of that meal (defined by the meal length), then that timestep is
+            # considered to be within one meal
+
             if (decimal_day < 1 / (n_meals + 1) + (meal_len * big_meal_extension / 60) / 24) & (
                     (1 == long_meal_1) | (1 == long_meal_2) | (1 == long_meal_3) | (1 == long_meal_4)):
                 meal_i = 1
@@ -240,6 +257,7 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
         # print(t, meal_i)
         meal_i, meal_len_extra = meal_definition_restricted(decimal_day, n_meals, meal_len)
 
+        # If the simulated time is between the start or end of a meal, pulse is active (> 0)
         if (t >= n_day + meal_i / (n_meals + 1)) & (
                 t <= (n_day + meal_i / (n_meals + 1) + (start_time_meal / 60) / 24)):
             pulse = pulse_i
@@ -265,7 +283,6 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
         return pulse, k_drop
 
 
-
     pulse, k_drop = meal_scheme(t, n_meals, meal_len, pulse_i, k_drop, regime, n_feedings)
 
     if feed <= 5.:
@@ -276,12 +293,13 @@ def dmi_rate_daily_scheme(state_vars, inputs, t):
 
     return feed_dt
 
-
+# Function that reads a pandas dataframe containing model inputs,
+# consecutively reads every row to be simulated into the ODE model,
+# and returns another pandas Dataframe with the simulation results.
 def multiple_simulation_timestep_per_row(model_inputs, model_def, initial_states, t_span):
     results = pd.DataFrame([])
     results['author_code'] = []
     results['trt_code'] = []
-    results['model_code'] = []
 
     results['feed'] = []
     results['time'] = []
@@ -335,19 +353,25 @@ def multiple_simulation_timestep_per_row(model_inputs, model_def, initial_states
 initial_states = [5.0]
 t_span = [0.,10.]
 
+
+
+# Definition of an example Dataframe of model inputs.
 model_inputs = pd.DataFrame([], columns=['author_code', 'trt_code', 'drop', 'pulse_dm', 'dm_availability', 'n_meals', 'meal_len', 'n_feedings' ])
 
-model_inputs.loc[0, 'author_code'] = 'example_1'
-model_inputs.loc[0, 'trt_code'] = 'adlib_8meals'
-model_inputs.loc[0, 'drop'] = 250.0
-model_inputs.loc[0, 'pulse_dm'] = 1700.0
-model_inputs.loc[0, 'dm_availability'] = 0.0
-model_inputs.loc[0, 'n_meals'] = 8.0
-model_inputs.loc[0, 'meal_len'] = 35.0
-model_inputs.loc[0, 'n_feedings'] = 2.0
+model_inputs.loc[0, 'author_code'] = 'example_1'    # Defines the code of publication. For this purpose, it's just example_1
+model_inputs.loc[0, 'trt_code'] = 'adlib_8meals'    # Defines the code of treatment within publication.
+model_inputs.loc[0, 'drop'] = 250.0                 # Defines the rate of DMI_rate decay to bring it down to the "zero intake"level. Always fixed.
+model_inputs.loc[0, 'pulse_dm'] = 1700.0            # Defines the pulse of DMI_rate at the beginning of the meal. Determined to fit daily DMI.
+model_inputs.loc[0, 'dm_availability'] = 0.0        # 0.0 is ad libitum; 1.0 is restricted feeding.
+model_inputs.loc[0, 'n_meals'] = 8.0                # Number of meals in a day.
+model_inputs.loc[0, 'meal_len'] = 35.0              # Lenght of a normal meal (not first meals post-feeding). Expressed in minutes.
+model_inputs.loc[0, 'n_feedings'] = 2.0             # Number of feedings offered in a day.
 
-print(model_inputs.to_string())
+
 results = multiple_simulation_timestep_per_row(model_inputs , dmi_rate_daily_scheme, initial_states, t_span)
+
+
+# Simple visualization of simulation
 
 plt.plot((results.time - 9.0) * 24.0, results.feed, color = 'black')
 plt.xlabel('Time, (hour)')
